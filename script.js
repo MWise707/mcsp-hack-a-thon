eb8d03c42452b895ee518e6fe024974122cd6a96
MWise707 <markezrawiseman@gmail.com>
2023-11-28 09:39:48 -0800

    Second commit - Add dropdown button

--- script.js
+++ script.js
@@ -1,30 +1,41 @@
-document.addEventListener("DOMContentLoaded", function() {
-const ctx = document.getElementById("myChart");
+document.addEventListener("DOMContentLoaded", function () {
+  const ctx = $("#myChart");
+  const body = $("body");
+  const chartBtn = $(".dropbtn");
+  const chartTypes = $(".dropdown-content");
+  let chartType = "bar";
 
-
-let newChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1,
+  // ====== INITIAL CHART FUNCTION ========
+  function createChart() {
+    let newChart = new Chart(ctx, {
+      type: chartType,
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+          },
+        ],
       },
-    ],
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
       },
-    },
-  },
-});
+    });
+  }
+
+  // Dropdown button event listener
+  chartBtn.on("click", function() {
+    chartTypes.toggleClass("show");
+  })
+
+  
 
 
-
-
-
+  createChart();
 });
